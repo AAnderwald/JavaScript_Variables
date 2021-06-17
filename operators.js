a61a36f9d0f772f9ef161fd5bd4d209bb6c0a4a1
Anna Anderwald <anna.v.anderwald@gmail.com>
2021-06-17 16:17:45 -0400

    JavaScript Operators Exercise completed

---
+++ operators.js
@@ -0,0 +1,45 @@
+let a = 20;
+let b = 40;
+
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+//console.log (a); // 20
+//console.log (b); // 40
+//console.log (add); // 60
+//console.log (minus); // -20
+//console.log (multiply); // 800
+//console.log (dividing); // 0.5
+
+//let c = 11;
+//let str = "11";
+//let str2 = "eleven";
+//let isPresent = true;
+//let firstName = "Jackie";
+//let lastName = "Chan";
+
+//console.log (c + str); // 1111
+//console.log (c + str2); // 11eleven
+//console.log (c + isPresent); // 12
+//console.log (c + firstName); // 11Jackie
+//console.log (c + lastName); // 11Chan
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+console.log (d == str3); //true
+console.log (d === str3); //false
+console.log (!isPresent2); //true
+console.log ("eleven" == str4 && d >=str3); //false
+console.log (!isPresent2 || isPresent2); //true
+console.log (0 == false); //true
+console.log (0 === false); //false
+console.log (0 != false); //false
+console.log (0 !== false); //true
+
